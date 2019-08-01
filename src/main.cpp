0bfc38f4eecb5e80e33678f1da361eaa16de0d76
lingfengli <lingfeng.li@vanderbilt.edu>
2019-08-01 15:23:06 -0500

    setup makefile

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Lingfeng Li on 8/1/19.
+//
+#include "../include/tmp.h"
+#include <iostream>
+int main(){
+    TMP x;
+    std::cout<<x.toString();
+}
---
+++ src/tmp.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Lingfeng Li on 8/1/19.
+//
+
+#include "../include/tmp.h"
+
+TMP::TMP():x(1) {}
+
+std::string TMP::toString() {
+    return std::to_string(x);
+}
