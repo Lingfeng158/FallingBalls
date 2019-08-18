347402601fb1f75dd563b8afbc1d1b8e20418754
lingfengli <lingfeng.li@vanderbilt.edu>
2019-08-18 17:42:09 -0500

    unsafe multithreading

--- Tracer/main.cpp
+++ Tracer/main.cpp
@@ -6,6 +6,7 @@
 #include <dirent.h>
 #include <iostream>
 #include <vector>
+#include <thread>
 
 /**
  * enter directory name and return list of dir in vector
@@ -28,13 +29,45 @@ std::vector<std::string> listdir(std::string dir){
     return result;
 }
 
+/**
+ * creates tif Images using tracer
+ * supported for multithreading
+ * single thread uses defualt parameter
+ * @param procNo the No. of current running thread
+ * @param totalProc total No. of running divisions
+ */
+void createImg(std::vector<std::string> dirList, int procNo=0, int totalProc=1){
+
+    int totalSize=dirList.size();
+    int dividedSize=totalSize/totalProc+1;
+    for(int index=procNo*dividedSize;index<(procNo+1)*dividedSize;index++){
+        if(index<totalSize){
+            std::cout<<"from thread NO: "<<procNo<<std::endl;
+            std::string cmd="../Tracer/tracer read ../txt/"+ (dirList[index]);
+            std::system(cmd.c_str());
+        }
+    }
+//    for(std::vector<std::string>::iterator it=a.begin();it!=a.end();it++){
+//        std::string cmd="../Tracer/tracer read ../txt/"+ (*it);
+//        std::system(cmd.c_str());
+//    }
+//    std::cout<<procNo;
+}
+
 int main(int argc, char** argv){
     std::string dir="../txt";
     std::vector<std::string> a=listdir(dir);
-    for(std::vector<std::string>::iterator it=a.begin();it!=a.end();it++){
-        std::string cmd="../Tracer/tracer read ../txt/"+ (*it);
-        std::system(cmd.c_str());
+    //get max thread count
+    //-1 for not slow down computer
+    int maxThread=std::thread::hardware_concurrency()-1;
+    //need to spawn maxThread-1
+    std::vector<std::thread> threads;
+    for(int i=0;i<maxThread-1;i++){
+        threads.push_back(std::thread(createImg, a, i, maxThread));
     }
+    createImg(a, maxThread-1, maxThread);
+    for(auto& t:threads)
+        t.join();
     //create video.mp4 at parent folder
     std::system("ffmpeg -framerate 25 -i ../Images/%04d.tif ../video.mp4");
 }
